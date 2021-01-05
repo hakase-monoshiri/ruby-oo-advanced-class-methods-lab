64b7411843c0a50265e82617b4307a079988e670
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-01-05 11:23:29 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
 
   def self.create
     new_song = Song.new
-    @@all << new_song
+    new_song.save
     new_song
   end
     
@@ -25,9 +25,35 @@ class Song
   end
 
   def self.create_by_name(name)
-    created_song = Song.new
-    created_song.name = name
+    created_song = Song.new_by_name(name)
     created_song.save
+    created_song
   end
+
+  def self.find_by_name(name)
+    self.all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    new_song = self.find_by_name(name)
+    if new_song == nil
+      new_song = self.create_by_name(name)
+    end
+    new_song
+  end
+
+  def self.alphabetical
+    self.all.sort_by{|song| song.name}
+  end
+
+  def self.new_from_filename(filename)
+    new_song = Song.new
+    name_and_artist = filename.split(/[.-]/)
+    name_and_artist[0] = new_song.artist_name
+    name_and_artist[2] = new_song.name
+    binding.pry
+    new_song
+  end
+
 
 end
