2576d10fbec16958df2caaf7ee97a37a54105d58
hakase-monoshiri <ebenaeleazer@outlook.com>
2021-01-05 10:22:51 -0500

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -8,6 +10,24 @@ class Song
 
   def save
     self.class.all << self
+  end
+
+  def self.create
+    new_song = Song.new
+    @@all << new_song
+    new_song
+  end
+    
+  def self.new_by_name(name)
+    new_song = Song.new
+    new_song.name = name
+    new_song
+  end
+
+  def self.create_by_name(name)
+    created_song = Song.new
+    created_song.name = name
+    created_song.save
   end
 
 end
